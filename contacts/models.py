d75a81b3f06f4c2f57e4ba7c5f7ab79db82bb7f4
roanblanchard <roan.g.blanchard@icloud.com>
2021-06-17 16:09:31 -0400

    half way functioning note

--- contacts/models.py
+++ contacts/models.py
@@ -2,9 +2,20 @@ from django.db import models
 from django.core.validators import RegexValidator
 from localflavor.us.models import USStateField, USZipCodeField
 from django.db.models.fields import DateField
+from datetime import datetime
+
+
+class Note(models.Model):
+    text = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Contact(models.Model):
+    note = models.ForeignKey(
+        Note,
+        on_delete=models.CASCADE,
+        null=True, blank=True
+    )
     phone_regex = RegexValidator(
         regex=r'^\+?\d{10}$',
         message="Phone number must be entered in the format: '+9999999999'.")
@@ -21,3 +32,8 @@ class Contact(models.Model):
     state = USStateField(null=True, blank=True)
     zip_code = USZipCodeField(null=True, blank=True)
     birthday = DateField(null=True, blank=True)
+
+
+
+
+
